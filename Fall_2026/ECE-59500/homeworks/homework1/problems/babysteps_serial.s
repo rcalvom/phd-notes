921e7845a7b47e18c696e1f329c20ca863ed1b97
Ricardo Calvo <ricardo.andres.calvo@gmail.com>
2026-09-02 18:46:12 -0400

    Document ARM debugging workflow

--- Fall_2026/ECE-59500/homeworks/homework1/problems/babysteps_serial.s
+++ Fall_2026/ECE-59500/homeworks/homework1/problems/babysteps_serial.s
@@ -1,6 +1,5 @@
 .equ UART1_UTXD, 0x02020040
 
-.section .text
 .global _start
 
 _start:
@@ -10,12 +9,9 @@ _start:
 print_character:
     ldrb r2, [r0], #1
     cmp r2, #0
-    beq finished
+    beq .
     str r2, [r1]
     b print_character
-
-finished:
-    b finished
 
 message:
     .asciz "Hello from serial0!\r\n"
